4178520658c00c4b3474f5cdebbcd03c1f913b5c
Steven Fackler <sfackler@gmail.com>
2015-03-25 18:44:37 +0000

    rebuild pages at 6d15621c2b659127f989f63001bda4bc884d7a93

--- doc/implementors/core/error/trait.Error.js
+++ doc/implementors/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['openssl'] = ["<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='enum' href='openssl/ssl/error/enum.SslError.html' title='openssl::ssl::error::SslError'>SslError</a>",];
+implementors['openssl'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='enum' href='openssl/ssl/error/enum.SslError.html' title='openssl::ssl::error::SslError'>SslError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
