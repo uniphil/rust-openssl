717babc0b5774a72e7688c468ec4b5074c7188b0
Steven Fackler <sfackler@gmail.com>
2016-07-01 18:49:42 -0400

    Release v0.7.14

---
+++ doc/v0.7.14/implementors/core/iter/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['openssl'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='openssl/x509/struct.GeneralNames.html' title='openssl::x509::GeneralNames'>GeneralNames</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.7.14/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['openssl'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='openssl/x509/struct.GeneralNamesIter.html' title='openssl::x509::GeneralNamesIter'>GeneralNamesIter</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
