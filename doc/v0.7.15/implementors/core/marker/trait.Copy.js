d63107cc10082379c9ca95e503fb9d439bf98219
Steven Fackler <sfackler@gmail.com>
2016-08-11 21:12:12 -0700

    Release openssl-sys v0.7.15, openssl v0.8.0

---
+++ doc/v0.7.15/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["openssl_sys"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.BIO_METHOD.html' title='openssl_sys::BIO_METHOD'>BIO_METHOD</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.EVP_MD_CTX.html' title='openssl_sys::EVP_MD_CTX'>EVP_MD_CTX</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.EVP_CIPHER.html' title='openssl_sys::EVP_CIPHER'>EVP_CIPHER</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.HMAC_CTX.html' title='openssl_sys::HMAC_CTX'>HMAC_CTX</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.GENERAL_NAME.html' title='openssl_sys::GENERAL_NAME'>GENERAL_NAME</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.X509V3_CTX.html' title='openssl_sys::X509V3_CTX'>X509V3_CTX</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='openssl_sys/struct.BIGNUM.html' title='openssl_sys::BIGNUM'>BIGNUM</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
